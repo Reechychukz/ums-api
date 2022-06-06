fc35618d8978b0aa2cc27142dbc24b0cf54c599f
Richard Chukwuma <richardchukwuma99@gmail.com>
2022-06-06 10:09:12 +0100

    remove unused controller

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,33 +1,33 @@
-const {crudsOperations: crusdOperations} = require('../models/usermodel');
-function showError(err){
-    if(err) console.log(err);
-}
-function get(req, res, next){
-    crusdOperations.find({}, '', function(err, records){
-        showError(err);
-        res.json({
-            successful: true,
-            records
-        })
-    });
-}
+// const {crudsOperations: crusdOperations} = require('../models/usermodel');
+// function showError(err){
+//     if(err) console.log(err);
+// }
+// function get(req, res, next){
+//     crusdOperations.find({}, '', function(err, records){
+//         showError(err);
+//         res.json({
+//             successful: true,
+//             records
+//         })
+//     });
+// }
 
-function handlePost(request, response){
-    const{name, course, year} = request.body;
-    const addUser = new crusdOperations({
-        name,
-        course,
-        year
-    });
-    addUser.save(function(err, newRecord){
-        showError(err);
-        response.json({
-            successful: true,
-            newRecord
-        })
-    });
+// function handlePost(request, response){
+//     const{name, course, year} = request.body;
+//     const addUser = new crusdOperations({
+//         name,
+//         course,
+//         year
+//     });
+//     addUser.save(function(err, newRecord){
+//         showError(err);
+//         response.json({
+//             successful: true,
+//             newRecord
+//         })
+//     });
 
-    response.send("This is a post request");
-}
+//     response.send("This is a post request");
+// }
 
-module.exports = {get, handlePost};
+//module.exports = {get, handlePost};
